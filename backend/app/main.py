8ba66ecc8e73e204eda469c0975b1129fbd5f1be
tommaso_site <jeanyvestommaso@gmail.com>
2026-08-20 14:45:36 +0100

    Fix CORS for Netlify frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,9 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://chatartist.netlify.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
